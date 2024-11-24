276c64aa702367d079e27b9ebf00181bbd471c46
Bendzina <165944559+Bendzina@users.noreply.github.com>
2024-11-24 22:33:32 +0400

    init

--- books/urls.py
+++ books/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'books'
+
 urlpatterns = [
     path('', views.book_list, name='book_list'),
-    path('book/<int:pk>/', views.book_detail, name='book_detail'),
+    path('book/detail/<int:pk>/', views.book_detail, name='book_detail'),
 ]
 
 urlpatterns += [
